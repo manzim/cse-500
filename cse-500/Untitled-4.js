e8e0ba29d3e2bb99db986c57112671105710e08f
Ahmed Manzim Ridwan Soumik <manzim.ruet@gmail.com>
2017-12-15 22:23:14 +0600

    cse-500
    a project course of dept based on html, css, javascript.

---
+++ cse-500/Untitled-4.js
@@ -0,0 +1,12 @@
+
+<script type="text/javascript">
+
+var slideimages = new Array() // create new array to preload images
+slideimages[0] = new Image() // create new instance of image object
+slideimages[0].src = "firstcar.gif" // set image src property to image path, preloading image in the process
+slideimages[1] = new Image()
+slideimages[1].src = "secondcar.gif"
+slideimages[2] = new Image()
+slideimages[2].src = "thirdcar.gif"
+
+</script>
